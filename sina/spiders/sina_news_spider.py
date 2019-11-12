521b87c93e109955a3855297a1e14fff8b33b867
kevin <1633183459@qq.com>
2019-11-13 00:49:03 +0800

    add extract title feature but still some of title can not be selected

--- sina/spiders/sina_news_spider.py
+++ sina/spiders/sina_news_spider.py
@@ -6,9 +6,31 @@ import re
 class sina(scrapy.Spider):
     name = "sina"
     start_urls = ['https://news.sina.com.cn/']
+    custom_settings = {
+        "LOG_LEVEL": "ERROR"
+    }
 
     def parse(self, response):
         soup = BeautifulSoup(response.body, "lxml")
         tags = soup.findAll("a", href=re.compile(r"sina.*\d{4}-\d{2}-\d{2}.*shtml$"))
         for tag in tags:
-            print(f"{tag.text.strip()} {tag.get('href')}")
+            url = tag.get("href")
+            yield scrapy.Request(url, callback=self.parse_detail)
+
+    def parse_detail(self, response):
+        try:
+            soup = BeautifulSoup(response.body, "lxml")
+            title = self.extract_title(soup)
+            print(f"{title} {response.url}")
+            if title is None:
+                raise Exception(f"title not found {response.url}")
+        except Exception as e:
+            self.logger.error(str(e))
+
+    @staticmethod
+    def extract_title(soup):
+        selectors = ["h1.main-title", "h1#main-title", "h1#artibodyTitle", "h1.l_tit", "span.location h1"]
+        for selector in selectors:
+            if len(soup.select(selector)) != 0:
+                title = soup.select(selector)[0].text
+                return title
